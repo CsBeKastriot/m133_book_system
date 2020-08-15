1b4fbe2f033595ac379560788f5760684cba21e2
CsBeKastriot <Kastriot.Dulla@CsBe.ch>
2020-08-15 11:46:54 +0200

    Development Branch including - static - css and js libraries - essential files

--- m133_book_system/settings.py
+++ m133_book_system/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
